c7ce7538826fc4552f9e8fdd491d90f200f9a2e7
Koza <kiskoza@gmail.com>
2019-11-18 22:19:29 +0100

    Small changes to sample

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,27 @@ const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
-const int led = D0;
+const int led = D4;
 
 void handleRoot() {
-  digitalWrite(led, 1);
+  digitalWrite(led, 0);
   String msg = "Hello from ASP";
   IndexHtml page = IndexHtml(msg);
   server.send(200, "text/html", page.render());
   delay(500);
-  digitalWrite(led, 0);
+  digitalWrite(led, 1);
 }
 
 void handleJS() {
-  digitalWrite(led, 1);
+  digitalWrite(led, 0);
   MainJs page = MainJs();
   server.send(200, "text/javascript", page.render());
   delay(500);
-  digitalWrite(led, 0);
+  digitalWrite(led, 1);
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
+  digitalWrite(led, 0);
   String args = "";
   for (uint8_t i = 0; i < server.args(); i++) {
     args += " " + server.argName(i) + ": " + server.arg(i) + "\n";
@@ -41,7 +41,12 @@ void handleNotFound() {
   String uri = server.uri();
   ErrorHtml page = ErrorHtml(args, method, uri);
   server.send(404, "text/html", page.render());
+  delay(250);
+  digitalWrite(led, 1);
+  delay(250);
   digitalWrite(led, 0);
+  delay(250);
+  digitalWrite(led, 1);;
 }
 
 void setup(void) {
